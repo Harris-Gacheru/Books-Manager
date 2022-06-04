ad175fc6d3ef25ec26c48578a2c766d7873db00f
Harris <harriskarume.hk@gmail.com>
2022-06-05 01:54:41 +0300

    Intialize books controller

---
+++ backend/dist/controller/books.controller.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deleteBook = exports.updateBook = exports.getBook = exports.getBooks = exports.createBook = void 0;
+const createBook = () => {
+};
+exports.createBook = createBook;
+const getBooks = () => {
+};
+exports.getBooks = getBooks;
+const getBook = () => {
+};
+exports.getBook = getBook;
+const updateBook = () => {
+};
+exports.updateBook = updateBook;
+const deleteBook = () => {
+};
+exports.deleteBook = deleteBook;
+//# sourceMappingURL=books.controller.js.map
