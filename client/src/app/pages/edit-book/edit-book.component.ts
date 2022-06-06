332b5548b021a25d02a95d93bef3b9308afccc1a
Harris <harriskarume.hk@gmail.com>
2022-06-06 19:36:40 +0300

    Modify ui

--- client/src/app/pages/edit-book/edit-book.component.ts
+++ client/src/app/pages/edit-book/edit-book.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Book } from 'src/app/interfaces/book';
 import { BooksService } from 'src/app/services/books.service';
 
 @Component({
@@ -9,26 +10,46 @@ import { BooksService } from 'src/app/services/books.service';
   styleUrls: ['./edit-book.component.css']
 })
 export class EditBookComponent implements OnInit {
-  editForm = this.fb.group({
-    pages: [ 0, Validators.required],
-    image: ['', Validators.required]
-  })
-
+  book!: Book
   id: string = ''
   msg: string = ''
-
+  error: string = ''
+  editForm!: FormGroup 
+  
   constructor(private fb: FormBuilder, private booksService: BooksService, private route: ActivatedRoute, private router: Router) { }
-
+  
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.id = params['id']
+      
+      this.booksService.getBook(this.id).subscribe(data => {
+        this.book = data
+
+        this.editForm = this.fb.group({
+          name: [this.book.name, Validators.required],
+          pages: [this.book.pages , Validators.required],
+          image: [this.book.image],
+          author: [this.book.author, Validators.required],
+          description: [this.book.description, Validators.required]
+        })
+      })
     })
   }
 
   onSubmit(){
    console.log( this.editForm.value.image)
-   this.booksService.updateBook(this.id, this.editForm.value).subscribe(info => {
-      console.log(info)      
-   })
+   this.booksService.updateBook(this.id, this.editForm.value).subscribe(
+    (res) => {
+      this.msg = res.message
+      this.error = ''
+        
+      setTimeout(() => {
+        this.router.navigate([`/books/${this.id}`])          
+      }, 1200);
+    },
+    (error) => {
+      this.error = error.error
+    }
+   )
   }
 }
