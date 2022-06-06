1fc72806535e247b8d0f4b23fc9d1f7cef6da5df
Harris <harriskarume.hk@gmail.com>
2022-06-06 15:56:42 +0300

    add cors

--- backend/server.ts
+++ backend/server.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import router from './routes/books.routes'
 import auth_router from './routes/users.routes'
+import cors from 'cors'
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use('/bm', router)
 app.use('/bm', auth_router)
 
