31f6a09ce180bbb74d8b6f6c5d187d2e52fc2503
Harris <harriskarume.hk@gmail.com>
2022-06-06 21:17:30 +0300

    Update interceptor

--- client/src/app/services/jwtinterceptor.service.ts
+++ client/src/app/services/jwtinterceptor.service.ts
@@ -1,6 +1,6 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
 import { AuthService } from './auth.service';
 
 @Injectable({
@@ -11,13 +11,16 @@ export class JwtinterceptorService implements HttpInterceptor {
   constructor(private authService: AuthService) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token = this.authService.getToken()
+    const token = this.authService.getToken() || ""
     
-    if (token) {
       const tokenizedrequest = req.clone({headers: req.headers.set('token', token)})
       
-      return next.handle(tokenizedrequest)
-    }
-    throw new Error('Method not implemented.');
+      return next.handle(tokenizedrequest).pipe(
+        catchError(err => {
+          const error = err.error.message || "Failed. Try again later";
+
+          return throwError(error)
+        })
+      )
   }
 }
