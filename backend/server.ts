aaec40ef1e1ee697a1fc27db8818d8f15e544f15
Harris <harriskarume.hk@gmail.com>
2022-07-14 09:00:51 +0300

    netlify-lambda & serverless

--- backend/server.ts
+++ backend/server.ts
@@ -2,16 +2,21 @@ import express from 'express'
 import router from './routes/books.routes'
 import auth_router from './routes/users.routes'
 import cors from 'cors'
+import serverless from 'serverless-http'
 
 const app = express()
+const apiRoute = '/.netlify/functions/server/api'
 
 app.use(express.json())
 app.use(cors())
-app.use('/bm', router)
-app.use('/bm', auth_router)
 
-const PORT = 5690
+app.use(apiRoute, router)
+app.use(apiRoute, auth_router)
 
-app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}`)
-})
+// const PORT = 5690
+
+// app.listen(PORT, () => {
+//     console.log(`App running on port ${PORT}`)
+// })
+
+module.exports.handler = serverless(app)
