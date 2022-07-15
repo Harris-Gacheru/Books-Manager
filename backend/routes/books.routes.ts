10cd9fd809cab45c6714fb5b3c9baf163936cdf3
Harris-Gacheru <96842423+Harris-Gacheru@users.noreply.github.com>
2022-07-15 07:49:45 +0300

    Update books.routes.ts

--- backend/routes/books.routes.ts
+++ backend/routes/books.routes.ts
@@ -5,7 +5,7 @@ import { verifyToken } from '../middleware/verifytoken'
 const router = express.Router()
 
 router.post('/create', verifyToken, createBook)
-router.get('/books', verifyToken, getBooks)
+router.get('/books', getBooks)
 router.get('/books/:id', verifyToken, getBook)
 router.patch('/books/:id', verifyToken, updateBook)
 router.delete('/books/:id', verifyToken, deleteBook)
