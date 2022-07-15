cc827464db43d6adaf2515c846f125674df8323a
Harris <harriskarume.hk@gmail.com>
2022-07-15 23:59:25 +0300

    remove netlify configs and update apis

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -7,16 +7,14 @@ const express_1 = __importDefault(require("express"));
 const books_routes_1 = __importDefault(require("./routes/books.routes"));
 const users_routes_1 = __importDefault(require("./routes/users.routes"));
 const cors_1 = __importDefault(require("cors"));
-const serverless_http_1 = __importDefault(require("serverless-http"));
 const app = (0, express_1.default)();
-const apiRoute = '/.netlify/functions/server/api';
+const apiRoute = '/api';
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
 app.use(apiRoute, books_routes_1.default);
 app.use(apiRoute, users_routes_1.default);
-// const PORT = 5690
-// app.listen(PORT, () => {
-//     console.log(`App running on port ${PORT}`)
-// })
-module.exports.handler = (0, serverless_http_1.default)(app);
+const PORT = 5690;
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
+});
 //# sourceMappingURL=server.js.map
