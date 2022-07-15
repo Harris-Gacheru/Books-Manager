2c85bc403928fe6f3855c1ef49def327e2867a03
Harris <harriskarume.hk@gmail.com>
2022-07-16 01:00:03 +0300

    Modify server

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -9,11 +9,12 @@ const users_routes_1 = __importDefault(require("./routes/users.routes"));
 const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 const apiRoute = '/api';
+const PORT = process.env.PORT || 5690;
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
+app.get('/', (req, res) => { res.send('Welcome to books manager'); });
 app.use(apiRoute, books_routes_1.default);
 app.use(apiRoute, users_routes_1.default);
-const PORT = process.env.PORT || 5690;
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}`);
 });
--- backend/server.ts
+++ backend/server.ts
@@ -5,14 +5,15 @@ import cors from 'cors'
 
 const app = express()
 const apiRoute = '/api'
+const PORT = process.env.PORT || 5690
 
 app.use(express.json())
 app.use(cors())
 
+app.get('/', (req, res) => {res.send('Welcome to books manager')})
 app.use(apiRoute, router)
 app.use(apiRoute, auth_router)
 
-const PORT = process.env.PORT || 5690
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}`)
