3f180b6d060492520f6f8536f58dcf262688ba8b
Harris <harriskarume.hk@gmail.com>
2022-07-16 09:59:54 +0300

    Tracking

--- backend/routes/books.routes.ts
+++ backend/routes/books.routes.ts
@@ -1,10 +1,25 @@
 import express from 'express'
+import mssql from 'mssql'
+import sqlConfig from '../config/sqlconfig'
 import { createBook, deleteBook, getBook, getBooks, updateBook } from '../controller/books.controller'
 import { verifyToken } from '../middleware/verifytoken'
 
 const router = express.Router()
 
-router.get('/', (req, res) => {res.send('Get books')})
+router.get('/', async (req, res) => {
+    res.send('Getting books')
+    try {
+        let pool = await mssql.connect(sqlConfig)
+        const books = await pool.request().execute('getbooks')
+
+        if (!books.recordset[0]) {
+            res.json({message: 'No books available'})
+        }
+        res.json(books.recordset).send('Books')
+    } catch (error: any) {
+        res.json({error: error.message})
+    }
+})
 router.post('/create', verifyToken, createBook)
 router.get('/books', getBooks)
 router.get('/books/:id', verifyToken, getBook)
